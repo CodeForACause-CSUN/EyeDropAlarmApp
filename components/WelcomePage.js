e26f154c8ff69677f96e1c8976ce446853fdbfee
Nancy <chidifoma@gmail.com>
2023-01-22 11:40:04 -0800

    Added comments to each section

--- components/WelcomePage.js
+++ components/WelcomePage.js
@@ -1,24 +1,24 @@
-import React, {useState} from 'react';
+import React, {useState} from 'react'; //import statement
 import { View, Text, StyleSheet, Button, Modal, Linking } from 'react-native';
 
-export default function WelcomePage () {
+export default function WelcomePage () { //welcome page settings
   const [visibility, setVisibility] = useState(true);
       let hour= new Date().getHours()  
 
     let greeting = '';
-    if (hour < 12) {
+    if (hour < 12) { //calculations for morning hours 12 midnight - 11am
       greeting = 'Good Morning';
-    } else if (hour < 18) {
+    } else if (hour < 18) { // calculation afternoon hours 12noon - 5pm
       greeting = 'Good Afternoon';
-    } else {
+    } else { //evening hours 6pm - 12 midnight
       greeting = 'Good Evening';
     }
 
-    if (!visibility) {
+    if (!visibility) { //visibility variable styling 
       return null;
     }
 
-    return ( 
+    return ( //welcome page stylings 
       <Modal style={styles.container}>
         <View style={styles.containerText}>
           <View style={styles.topText}>
@@ -30,7 +30,7 @@ export default function WelcomePage () {
             <Text style={styles.paragraph}>Use the <Text style={styles.strong}>HELP</Text> button for tips and tutorials</Text>
           </View>
           <View style={styles.help}>
-            <Button title="HELP" color='blue' onPress={() => {
+            <Button title="HELP" color='blue' onPress={() => { //help and continue button styling
               Linking.openURL('https://www.eyedropalarm.com/how-to-put-in-eyedrops.html');
             }} />
             <Button title="CONTINUE" color="#000000" onPress={() => {setVisibility(false)}} />
@@ -40,7 +40,7 @@ export default function WelcomePage () {
     );
   }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({ //style sheet for each section
   container: {
     flex: 1,
     backgroundColor: '#ebebeb',
